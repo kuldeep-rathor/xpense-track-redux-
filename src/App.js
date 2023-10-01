f7746644b1bc5ed609da88958e5c0597df3da779
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2023-10-01 21:18:26 +0530

    Footer-update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/header";
 import Home from "./pages/home";
 import AddExpense from "./pages/add-expense";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/add-expense" component={AddExpense} />
       </Switch>
-      <div>Footer</div>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/components/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return <footer className="footer"> Created By Kuldeep-Rathor 😉 </footer>;
+};
+
+export default Footer;
